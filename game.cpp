646c6458efed09297b0dc11ce81451451ee531f4
Infinityworks <107305997+DiamondPixel4k@users.noreply.github.com>
2022-11-21 01:46:14 +0000

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -1,8 +1,6 @@
 #include <ncurses.h>
 #include <unistd.h>
-
 #define DELAY 50000
-
 using namespace std;
 
 void setup() {
@@ -11,32 +9,28 @@ void setup() {
 	curs_set(FALSE);
 }
 
-int main(int argc, char *argv[]) {
-	int x = 0, y = 0;
-	int maxX = 0, maxY = 0;
-	int nextX = 0;
-	int direction = 1;
+int main(int argc, char *argv[])
+{
+int x = 0, y = 0;
+int maxX = 0, maxY = 0;
+int nextX = 0;
+int direction = 1;
+setup();
 
-	setup();
-
-
-	while(1) {
-		getmaxyx(stdscr,maxY,maxX);
-
-		clear();
-		mvprintw(y,x,"o");
-		refresh();
-
-		usleep(DELAY);
-		nextX = x + direction;
-
-		if (nextX >= maxX || nextX < 0) {
-			direction *= -1;
-		}
-		else {x += direction;}
+while(1)
+{
+	getmaxyx(stdscr,maxY,maxX);
+	clear();
+	mvprintw(y,x,"o");
+	refresh();
+	usleep(DELAY);
+	nextX = x + direction;
+		
+	if (nextX >= maxX || nextX < 0) {
+		direction *= -1;
 	}
-
-	endwin();
-
-	return 0;
+	else {x += direction;}
+}
+endwin();
+return 0;
 }
